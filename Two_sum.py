4f2d33385bc314d08431104fba3d97461afdad96
Tutu Daranijo <tutudaranijo@gmail.com>
2023-08-08 22:08:14 -0400

    reverse commit

---
+++ Two_sum.py
@@ -0,0 +1,10 @@
+class solution:
+    def twosum(self, nums: list[int], target: int) -> list[int]:
+        prevMap = {}
+
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in prevMap:
+                return [prevMap[diff], i]
+            prevMap[n] = i
+        return
