2b1d26c7c39ed83799adbff0e55c3d26e4e9ab01
tutudaranijo <94590829+tutudaranijo@users.noreply.github.com>
2024-07-04 20:15:21 -0500

    1 stack

---
+++ EvaluateReversePolishNotation.py
@@ -0,0 +1,31 @@
+class Solution:
+    def evalRPN(self, tokens: list[str]) -> int:
+
+        stack = []
+
+        for char in tokens:
+
+            if char == "+":
+
+                stack.append(stack.pop() + stack.pop())
+            
+            elif char == "-":
+                # different because the order
+                a,b=stack.pop(), stack.pop()
+                
+                stack.append(b-a)
+            
+
+            elif char == "*":
+                stack.append(stack.pop() * stack.pop())
+
+            elif char == "/":
+                a,b=stack.pop(),stack.pop()
+                stack.append(int(b/a))
+                
+            
+            else:
+                stack.append(int(char))
+
+        return stack[0]
+            
